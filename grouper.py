fecf40212a5f59f5c6a675d77c0b769dee279e7e
btuladhar22 <btuladhar22@my.whitworth.edu>
2021-04-19 22:29:39 -0700

    Completed

--- grouper.py
+++ grouper.py
@@ -35,18 +35,20 @@ def grouping(propPref, accepPref):
     return match
 
 
-# group1 = {
-#     0: [0,3,2,1],
-#     1: [2,3,1,0],
-#     2: [0,2,1,3],
-#     3: [1,2,0,3]
-# }
-# group2 = {
-#     0: [0,1,2,3],
-#     1:[1,2,3,0],
-#     2:[1,3,0,2],
-#     3:[0,2,1,3]
-# }
-# result = grouping(group1, group2)
-# # the result is picked:picker
-# print(result)
+group1 = {
+     0: [0,3,2,1],
+     1: [2,3,1,0],
+     2: [0,2,1,3],
+     3: [1,2,0,3]
+ }
+
+group2 = {
+     0:[0,1,2,3],
+     1:[1,2,3,0],
+     2:[1,3,0,2],
+     3:[0,2,1,3]
+}
+ 
+result = grouping(group1, group2)
+ # the result is picked:picker
+ print(result)
